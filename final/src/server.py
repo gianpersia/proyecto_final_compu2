10287446ea5a3cfdd273bd9a6b3481d4a176cb47
gianpersia <g.persia@alumno.um.edu.ar>
2024-10-07 17:51:00 -0300

    Agrego manejo de subida

--- final/src/server.py
+++ final/src/server.py
@@ -9,23 +9,38 @@ import threading
 #Directorio donde se guardaran los archivos
 STORAGE_DIR = "/Users/gpersia/Documents/Facultad/proyecto_final_compu2/final" 
 
-class MyTCPHandler(socketserver.BaseRequestHandler):
+def handle_client(client_socket): #gestion de la interaccion con un cliente
+    try:
+        data = client_socket.recv(1024).decode()
+        command = data.strip().split()
+        operation = command[0]
 
-    def handle_client(client_socket): #gestion de la interaccion con un cliente
-        try:
-            data = client_socket.recv(1024).decode()
-            command = data.strip().split()
-            operation = command[0]
+        if operation == "upload":
+            handle_upload(command, client_socket)
+        elif operation == "download":
+            handle_download(command, client_socket)
+        elif operation == "list":
+            handle_list(client_socket)
+        else:
+            client_socket.send(b"Error: comando inexistente\n")
+    except Exception as e:
+        client_socket.send(f"Error: {str(e)}\n".encode())
 
-            if operation == "upload":
-                handle_upload(command, client_socket)
-            elif operation == "download":
-                handle_download(command, client_socket)
-            elif operation == "list":
-                handle_list(client_socket)
-            else:
-                client_socket.send(b"Error: comando inexistente\n")
-        except Exception as e:
-            client_socket.send(f"Error: {str(e)}\n".encode())
+    client_socket.close()
+    
+def handle_upload(command, client_socket): #gestion de subida de archivos
+    if len(command) < 2:
+        client_socket.send(b"Falta nombre archivo\n")
+        return
+    
+    filename = command[1]
+    file_path = os.path.join(STORAGE_DIR, filename)
 
-        client_socket.close()
+    with open(file_path, 'wb') as f:
+        while True:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            f.write(data)
+
+    client_socket.send(b"Archivo subido\n")
